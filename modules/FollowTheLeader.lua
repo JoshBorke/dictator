6086269b1f2bfc16722c98055d7b53437a06976a
Josh <joshborke+github@gmail.com>
2008-10-15 16:27:43 -0400

    update follow the leader a bit

--- modules/FollowTheLeader.lua
+++ modules/FollowTheLeader.lua
@@ -2,6 +2,15 @@ local modName = "FollowTheLeader"
 local dictator = LibStub("AceAddon-3.0"):GetAddon("Dictator")
 local mod = dictator:NewModule(modName, {}, "AceEvent-3.0", "AceComm-3.0")
 local player = UnitName("player")
+local leader
+
+local function GetUnit(name)
+	for i=1, GetNumPartyMembers() do
+		if UnitName(i) == leader then
+			return "party"..i
+		end
+	end
+end
 
 function mod:OnEnable()
 	self:RegisterEvent("PLAYER_REGEN_ENABLED")
@@ -15,11 +24,15 @@ end
 
 -- main addon takes care of any saved variables (what needs to be saved?)
 function mod:OnInitialize()
+	leader = GetUnit(dictator.db.leader)
 end
 
 -- don't cache the leader, the user might opt to change leaders
 function mod:PLAYER_REGEN_ENABLED()
-	FollowUnit(dictator.db.leader, 1)
+	leader = GetUnit(dictator.db.leader)
+	if (leader) then
+		FollowUnit(leader, 1)
+	end
 end
 
 function mod:AUTOFOLLOW_END()
@@ -31,4 +44,8 @@ function mod:OnCommReceived(prefix, message, distribution, sender)
 	if (prefix == "DIC" and distribution=="WHISPER" and player == dictator.db.leader) then
 		dictator:Warn(modName, sender.." stopped following")
 	end
+end
+
+function mod:UpdateLeader()
+	leader = GetUnit(dictator.db.leader)
 end
